295023401b2c6c091b19781424f80860addaac96
hh <h851149862@gmail.com>
2024-07-15 20:47:46 +0800

    feat: vite配置之静态资源处理

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // 自定义的环境变量
+  readonly VITE_IMG_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,50 @@ import autoprefixer from "autoprefixer";
 import UnoCSS from "unocss/vite";
 // 将SVG注册为组件
 import svgr from "vite-plugin-svgr";
+// 压缩图片资源
+import viteImagemin from "vite-plugin-imagemin";
 
 // 这个是做路径归一化处理的，修改路径中的斜线问题。使用的好像是绝对路径。线上的话要怎么办呢？
 const variablePath = normalizePath(path.resolve("./src/variable.scss"));
 
+// 填入项目的 CDN 域名地址
+const CDN_URL = "http://127.0.0.1:3000";
+
+// 是否为生产环境，在生产环境一般会注入 NODE_ENV 这个环境变量，见下面的环境变量文件配置
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProduction ? CDN_URL : "/",
   // 手动指定项目根目录位置，因为vite的入口文件为index.html文件
   // root: path.join(__dirname, "src"),
-  plugins: [react(), UnoCSS(), svgr()],
+  plugins: [
+    react(),
+    UnoCSS(),
+    svgr(),
+    viteImagemin({
+      // 无损压缩配置，无损压缩下图片质量不会变差，针对png，范围从 0 到 7。优化级别越高，压缩时间越长，但压缩效果也越好。
+      optipng: {
+        optimizationLevel: 7,
+      },
+      // pngquant 是一个用于 PNG 图片有损压缩的工具。有损压缩意味着图片质量可能会变差，但压缩效果更好。
+      pngquant: {
+        quality: [0.8, 0.9],
+      },
+      // svg 优化，没有使用svgr优化过的会进行这个优化
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       // 自动引入scss文件
@@ -39,5 +74,9 @@ export default defineConfig({
       "@assets": path.join(__dirname, "src/assets"),
       "@": path.join(__dirname, "src"),
     },
+  },
+  build: {
+    // 当静态资源超过临界值则提取成为单独的文件。
+    assetsInlineLimit: 8 * 1024,
   },
 });
